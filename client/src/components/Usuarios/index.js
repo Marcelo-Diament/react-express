5fefacd9de56e58ee1ca7272a97df44719728c2c
Marcelo-Diament <marcelodiament@gmail.com>
2020-12-04 12:15:10 -0300

    feature: initial component

---
+++ client/src/components/Usuarios/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+
+const Usuarios = () => {
+  return (
+    <section>
+      <h2>Usuários</h2>
+      <ul>
+        <li>#ID | Nome Sobrenome</li>
+        <li>#ID | Nome Sobrenome</li>
+        <li>#ID | Nome Sobrenome</li>
+        <li>#ID | Nome Sobrenome</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Usuarios
